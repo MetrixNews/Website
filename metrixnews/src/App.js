998d044733e1306233202d838a47fe62cca03896
AJ Raymond <antraymo@iu.edu>
2019-08-27 14:39:17 -0500

    started playing in React framework

---
+++ metrixnews/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Nav from './nav'
+import News_Spectrum from './news_spectrum'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <News_Spectrum />
+    </div>
+  );
+}
+
+export default App;
