e04675f1dcfb4bc1f1da7be836cd3a0f8d0a5d37
AJ Raymond <antraymo@iu.edu>
2019-10-01 20:29:10 -0500

    article summary component

--- metrixnews/src/components/article_summary.js
+++ metrixnews/src/components/article_summary.js
@@ -1,1 +1,17 @@
 import React, { Component }  from 'react';
+
+function ArticleContent(props) {
+    return (
+        <section>
+            <h4>{articleContent.sectionTitle}</h4>
+            <p className = "article_description">
+                {articleContent.text}
+            </p>
+        </section>
+    )
+}
+
+const articleContent = {
+    sectionTitle: 'Summary',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+};
--- metrixnews/src/index.js
+++ metrixnews/src/index.js
@@ -7,8 +7,14 @@ import * as serviceWorker from './serviceWorker';
 
 
 
+
+
+
   ReactDOM.render(
-    <Nav/>,
+    <ArticleContent
+        sectionTitle={articleContent.sectionTitle}
+        text={articleContent.text}
+    />,
     document.getElementById('root')
   );
 
@@ -16,3 +22,18 @@ import * as serviceWorker from './serviceWorker';
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+// function PageHeader(props) {
+//     return (
+//         <div className="pageDescription">
+//             <h1>{props.title}</h1>
+//             <p>{props.desc}</p>
+//         </div>
+//     )
+// }
+
+// const page_header = {
+//     title: 'NEWS EXPLORATION',
+//     desc: 'Browse todays hottest political topics from trusted news sources. Select any topic to analyze it more in depth. Click here to suggest a new topic or source. Visit the About page to learn how we label articles.'
+// };
