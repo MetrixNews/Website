0e80c6d24568dc4cc6526b3ee76b8fc784fb3234
AJ Raymond <antraymo@iu.edu>
2019-10-01 20:58:40 -0500

    newsspectrum fix

--- metrixnews/src/App.js
+++ metrixnews/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import Nav from './components/nav'
-import Spectrum from './components/news_spectrum'
 
 function App() {
   return (
     <div className="App">
-      {/* <Nav /> */}
-      <Spectrum />
+
     </div>
   );
 }
