5a40dd0d7cf94371d4d57d11638be287c06c7e92
AJ Raymond <antraymo@iu.edu>
2019-10-16 20:18:26 -0500

    political party styles

--- metrixnews/src/components/election/party/candidate_deck.js
+++ metrixnews/src/components/election/party/candidate_deck.js
@@ -1,18 +1,22 @@
 import React, { Component }  from 'react';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Candidate from '../candidate/candidate';
+import './party.scss'
 
 // Still needs new styling ... used React Bootstrap ... https://react-bootstrap.github.io/components/cards/#card-deck
 
 function PoliticalParty(props) {
     return (
-        <CardDeck>
-            <Candidate />
-            <Candidate />
-            <Candidate />
-            <Candidate />
-            <Candidate />
-        </CardDeck>
+        <div class="polticalParty">
+            <h2>Democrats:</h2>
+            <CardDeck>
+                <Candidate />
+                <Candidate />
+                <Candidate />
+                <Candidate />
+                <Candidate />
+            </CardDeck>
+        </div>
     )   
 }
 
