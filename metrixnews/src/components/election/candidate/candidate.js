72dbc409fce76cf408eda66b160cbbafda3f7bb3
AJ Raymond <antraymo@iu.edu>
2019-10-16 20:03:19 -0500

    candidate styling

--- metrixnews/src/components/election/candidate/candidate.js
+++ metrixnews/src/components/election/candidate/candidate.js
@@ -15,18 +15,20 @@ function Candidate(props) {
           <ul>
               <li>
                 <Link to="./components/candidatepage.js">
-                  <Card>
-                  <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/d/de/Bernie_Sanders.jpg" />
-                  <Card.Body>
-                      <Card.Text>
-                        <p>Bernie Sanders</p>
-                        <p>Democrat</p>
-                        <p>U.S. Senator</p>
-                        <p>Vermont</p>
-                        <p>"Not Me, Us"</p>
-                      </Card.Text>
-                    </Card.Body>
-                  </Card>
+                  <div className="candidate">
+                    <Card className="container">
+                    <Card.Img className="candidatePic" variant="top" src="https://upload.wikimedia.org/wikipedia/commons/d/de/Bernie_Sanders.jpg" />
+                    <Card.Body>
+                        <Card.Text className="content">
+                          <h3>Bernie Sanders</h3>
+                          <p>Democrat</p>
+                          <p>U.S. Senator</p>
+                          <p>Vermont</p>
+                          <p>"Not Me, Us"</p>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
                 </Link>
                 <Route exact path='./components/candidatepage.js' component={CandidateComponent}></Route>
               </li>
