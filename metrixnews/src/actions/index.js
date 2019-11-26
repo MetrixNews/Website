e7ad6be636ad927c7806d443c6157ced0a0625e4
AJ Raymond <antraymo@iu.edu>
2019-11-26 15:15:52 -0600

    cleaning

--- metrixnews/src/actions/index.js
+++ metrixnews/src/actions/index.js
@@ -1,32 +1,12 @@
 // Fetch Articles
-export const FETCH_ARTICLES_PENDING = 'FETCH_ARTICLES_PENDING';
-export const FETCH_ARTICLES_SUCCESS = 'FETCH_ARTICLES_SUCCESS';
-export const FETCH_ARTICLES_ERROR = 'FETCH_ARTICLES_ERROR';
+// export const FETCH_ARTICLES_PENDING = 'FETCH_ARTICLES_PENDING';
+// export const FETCH_ARTICLES_SUCCESS = 'FETCH_ARTICLES_SUCCESS';
+// export const FETCH_ARTICLES_ERROR = 'FETCH_ARTICLES_ERROR';
 
 export const FETCH_CATEGORIES_PENDING = 'FETCH_CATEGORIES_PENDING';
 export const FETCH_CATEGORIES_SUCCESS = 'FETCH_CATEGORIES_SUCCESS';
 export const FETCH_CATEGORIES_ERROR = 'FETCH_CATEGORIES_ERROR';
 
-
-export function fetchArticlesPending() {
-    return {
-        type: FETCH_ARTICLES_PENDING
-    }
-}
-
-export function fetchArticlesSuccess(articles) {
-    return {
-        type: FETCH_ARTICLES_SUCCESS,
-        articles: articles
-    }
-}
-
-export function fetchArticlesError(error) {
-    return {
-        type: FETCH_ARTICLES_ERROR,
-        error: error
-    }
-}
 
 export function fetchCategoriesPending() {
     return {
@@ -48,14 +28,29 @@ export function fetchCategoriesError(error) {
     }
 }
 
+// export function fetchArticlesPending() {
+//     return {
+//         type: FETCH_ARTICLES_PENDING
+//     }
+// }
+
+// export function fetchArticlesSuccess(articles) {
+//     return {
+//         type: FETCH_ARTICLES_SUCCESS,
+//         articles: articles
+//     }
+// }
+
+// export function fetchArticlesError(error) {
+//     return {
+//         type: FETCH_ARTICLES_ERROR,
+//         error: error
+//     }
+// }
 
 
-// ARTICLE_BIAS_PLACEMENT (left center right)
-// translate metrics into pics
 
-// ARTICLE_EMOTION_FILTER
-// ARTICLE_BIAS_FILTER
-// ARTICLE_SENTIMENT_FILTER
+
 
 
 
