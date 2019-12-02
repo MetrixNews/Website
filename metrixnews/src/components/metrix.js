dee3af04886fa2c6c583474c30f01986f299edc4
AJ Raymond <antraymo@iu.edu>
2019-12-01 23:36:29 -0600

    about sections

--- metrixnews/src/components/metrix.js
+++ metrixnews/src/components/metrix.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 
 import '../app.scss';
 
-function MetrixComponent (){
- return <h1>Metrix Component</h1>
-}
+class Metrix extends Component {
+    constructor(props) {
+        super(props);
+    }
+  
+    render() {
 
-export default MetrixComponent;
+        return (
+          <div className="about"> 
+              <h2>What We Do</h2>
+              <h2>How it Works</h2>
+              <h2>Our People</h2>
+          </div>
+        )
+    }
+  }
+
+export default Metrix;
