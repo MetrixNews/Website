7891576b4ca7a6566b3ff44a0be3d811f84d3367
AJ Raymond <antraymo@iu.edu>
2019-12-01 23:28:24 -0600

    structure

--- metrixnews/src/components/election/candidate.js
+++ metrixnews/src/components/election/candidate.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import Candidate from './candidate/candidate';
 
-function CandidateContainer (){
- return (
-    <Candidate />
- )
-}
-
-export default CandidateContainer;
+export default class Candidate extends Component {
+   constructor(props) {
+     super(props);
+   }
+ 
+   render() {
+     console.log(this.props)
+     return (
+       <div className="candidate">
+         <div className="candidateName">
+           <h2>Bernie Sanders</h2>
+         </div>
+         <Spectrum articles={this.props.topic}/>
+         <div className="campaignLinks">
+           <PrimaryButton/>
+           <PrimaryButton/>
+           <PrimaryButton/>
+         </div>
+       </div>
+       )
+     }
+ }
