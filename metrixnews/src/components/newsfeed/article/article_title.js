fdcb81a2c9c43de3c5fc26dbcc4d6de6bf9a2941
AJ Raymond <antraymo@iu.edu>
2019-12-31 14:35:51 -0600

    title style

--- metrixnews/src/components/newsfeed/article/article_title.js
+++ metrixnews/src/components/newsfeed/article/article_title.js
@@ -7,8 +7,21 @@ export default class ArticleTitle extends Component {
     }
 
     render() {
+
+        const headline = {
+            font: "Source Sans Pro",
+            color: "var(--article-font-color)",
+            fontSize: "12pt",
+            fontWeight: "bold",
+            marginTop: "5px",
+            marginBottom: "5px",
+            position: "relative",
+            textAlign: "left",
+
+        }
+
       return (
-        <div className="headline">
+        <div style={headline}>
             <span>{this.props.story.title}</span>
         </div>
     )
