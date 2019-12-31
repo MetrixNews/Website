117331e7139b770c44c505f38ceef33132296b54
AJ Raymond <antraymo@iu.edu>
2019-12-31 14:26:09 -0600

    articlehead styles

--- metrixnews/src/components/newsfeed/article/article_head.js
+++ metrixnews/src/components/newsfeed/article/article_head.js
@@ -1,4 +1,5 @@
 import React, { Component }  from 'react';
+import Moment from 'react-moment';
 import '../newsfeed.scss'
 
 export default class ArticleHead extends Component {
@@ -6,11 +7,9 @@ export default class ArticleHead extends Component {
         super(props);
     }
     render() {
-
-        const logo = {
-            width: "12%",
-            float: "left",
-            paddingBottom: "5px",
+        const row = {
+            width: "100%",
+            height: "auto",
         }
 
         const publishInfo = {
@@ -26,13 +25,9 @@ export default class ArticleHead extends Component {
             fontWeight: "bold",
         }
 
-        const time = {
-            fontStyle: "italic",
-        }
-
     return (
-        <div>
-            <div style={logo}>
+        <div style={row}>
+            <div>
                 <SourceLogo story={this.props.story}/>
             </div>
 
@@ -40,7 +35,7 @@ export default class ArticleHead extends Component {
                 <span style={source}>{this.props.story.source}</span><br/>
                 <span>{this.props.story.author}</span>
                 <span>   -   </span>    
-                <span style={time}>{this.props.story.published_at}</span>                                      
+                <PrettyDate story={this.props.story}/>                                    
             </div>
       </div>
     );
@@ -50,29 +45,49 @@ export default class ArticleHead extends Component {
 function SourceLogo(props) {
     const url = props.story.url;
 
+    const logo = {
+        width: "15%",
+        float: "left",
+        padding: "5px",
+    }
+
     if(url.search(url.match(".com")) >= 0) {
         return (
-            <img src={"https://logo.clearbit.com/" + url.substring(0,url.search(".com")+4)} className="source_logo"  alt="logo"/>
+            <img src={"https://logo.clearbit.com/" + url.substring(0,url.search(".com")+4)} style={logo}  alt="logo"/>
         )
     }
     if(url.search(url.match(".org")) >= 0) {
         return (
-            <img src={"https://logo.clearbit.com/" + url.substring(0,url.search(".org")+4)} className="source_logo"  alt="logo"/>
+            <img src={"https://logo.clearbit.com/" + url.substring(0,url.search(".org")+4)} style={logo}  alt="logo"/>
         )
     }
     if(url.search(url.match(".net")) >= 0) {
         return (
-            <img src={"https://logo.clearbit.com/" + url.substring(0,url.search(".net")+4)} className="source_logo"  alt="logo"/>
+            <img src={"https://logo.clearbit.com/" + url.substring(0,url.search(".net")+4)} style={logo}  alt="logo"/>
         )
     }
     if(url.search(url.match(".co.uk")) >= 0) {
         return (
-            <img src={"https://logo.clearbit.com/" + url.substring(0,url.search(".co.uk")+6)} className="source_logo"  alt="logo"/>
+            <img src={"https://logo.clearbit.com/" + url.substring(0,url.search(".co.uk")+6)} style={logo}  alt="logo"/>
         )
     }
     else {
         return (
-            <img src="https://via.placeholder.com/50x50" className="source_logo"  alt="logo"/>
+            <img src="https://via.placeholder.com/50x50" style={logo}  alt="logo"/>
         )
     }
   }
+
+  function PrettyDate(props) {
+    const time = props.story.published_at;
+
+    const timeStyle = {
+        fontStyle: "italic",
+    }
+        return (
+            <span style={timeStyle}>
+                <Moment fromNow ago>{time}</Moment>
+            </span>
+            
+        );
+    }
