f3d1a73c2857b275b9c1dbd92cb451c34a21e0da
AJ Raymond <antraymo@iu.edu>
2020-02-06 01:11:42 -0600

    delete form reducer

--- metrixnews/src/reducers/root.js
+++ metrixnews/src/reducers/root.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 
 import NewsfeedReducer from '../reducers/newsfeed';
 import ElectionReducer from '../reducers/election';
-import { reducer as formReducer } from 'redux-form';
 
 
 const RootReducer = combineReducers({
     newsfeed: NewsfeedReducer,
     election: ElectionReducer,
-    form: formReducer,
   })
 
   export default RootReducer;
