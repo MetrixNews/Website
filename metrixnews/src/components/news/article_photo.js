5d2b526d2f29ca346550cc1cf3abff7e0dcdf27f
AJ Raymond <antraymo@iu.edu>
2020-02-20 22:40:50 -0600

    Successful lazy load on intersection

--- metrixnews/src/components/news/article_photo.js
+++ metrixnews/src/components/news/article_photo.js
@@ -6,9 +6,27 @@ export default class ArticlePhoto extends PureComponent {
         super(props);
     }
 
+    componentDidMount() {
+        this.observer = new IntersectionObserver(
+            entries => {
+              entries.forEach(entry => {
+                const { isIntersecting } = entry;
+                if (isIntersecting) {
+                  this.element.src = this.props.photo;
+                  this.observer = this.observer.disconnect();
+                }
+              });
+            },
+            {
+              root: document.querySelector(".container")
+            }
+          );
+          this.observer.observe(this.element);
+    }
+
     render() {
       return (
-        <img className="articlephoto" src={this.props.photo} alt="" />
+        <img className="articlephoto" ref={el => this.element = el} />
     )
 }
 }
